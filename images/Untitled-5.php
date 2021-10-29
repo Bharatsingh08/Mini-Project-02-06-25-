483f251b3fb8eddb1a0d18c60c59737de67d4667
Barkha Rani <bt7149@srmist.edu.in>
2021-10-29 20:26:39 +0530

    adding files

---
+++ images/Untitled-5.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+	<body>
+		<h1>Please Login</h1>
+	<form id="loginform" name="loginform">
+		Username:<br>
+			<input type="text" name="username" id="username" form="loginform" title="username"><br>
+			Password:<br>
+			<input type="password" required="required" name="password" id="password" title="password" ><br>
+		<input name="login" type="submit" title="login" value="submit">
+	  </form>
+</body>
+</html>
